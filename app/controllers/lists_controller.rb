2e9e88543693d7fc0139d232e6aba394861d43ac
Augusto Schmidt <augusto_schmidt@outlook.com.br>
2023-05-25 17:39:05 -0300

    Solved Part 1

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,39 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmarks = @list.bookmarks
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list), notice: "List was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_url, notice: "List was successfully destroyed."
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+end
