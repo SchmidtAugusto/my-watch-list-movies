fe840f0c97f8668f68708305c7f4655321f50068
Augusto Schmidt <augusto_schmidt@outlook.com.br>
2023-05-29 13:04:25 -0300

    Solved

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,7 +2,7 @@ class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
     @list = List.find(params[:list_id])
-    @movies = Movie.all
+    @movies = Movie.all.order(:title)
   end
 
   def create
